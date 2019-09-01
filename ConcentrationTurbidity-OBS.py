caee57b5b814fd48342a025743e8be875f20ba76
Santiago <santiagoscheiner@gmail.com>
2019-08-31 22:23:50 -0300

    Agregado el ajuste correspondiente a la medición con el OBS.

--- ConcentrationTurbidity-OBS.py
+++ ConcentrationTurbidity-OBS.py
@@ -15,7 +15,7 @@ sys.path.append(path)
 
 TitleSize=30
 AxisLabelSize=15
-LegendSize=15
+LegendSize=12
 NumberSize=10
 
 plt.close('all')
@@ -34,51 +34,73 @@ for s in archivos:
     # Extraemos las cuatro columnas que nos interesan:
     y_df = pd.DataFrame(data, columns= ['Concentración'])
     
-    
-    y_df=np.array(y_df)
+    y_df = np.array(y_df)
     y_df = y_df[:12]
     
     x = pd.DataFrame(data, columns= ['Sample 1 (FNU)','Sample 2 (FNU)','Sample 3 (FNU)'])
-    x = x.where((pd.notnull(x)), None)
-    x=np.array(x)
-    x = x[:12,:]
+    x = x.dropna(axis=0)
+    x = np.array(x)
+    #x = x[:12,:]   # no tiene sentido seguir haciéndolo, porque  dropna() ya le sacó las nulas.
     
     x_med=np.zeros(len(x))
     x_err=np.zeros((2,len(x)))
     x_err_tot=np.zeros(len(x))
-    y=np.zeros(len(x))
+    
+    x_OBS_df = pd.DataFrame(data, columns= ['SS'])
+    x_OBS_df = np.array(x_OBS_df)
+    x_OBS_df = x_OBS_df[:12]
+    
+    y_OBS=np.zeros(len(y_df))
+    x_OBS=np.zeros(len(y_df))
+    xerr_OBS=10*np.ones(len(x_OBS))
+    
+    y_Hach=[486.0606061, 324.040404, 241.0703812, 158.9673315, 118.9453447, 79.11100286, 0] # A mano. Ya fue.
     
     for i in range(len(x)):
        x_med[i]=np.median(x[i])
        x_err[0,i]=x_med[i] - np.min(x[i])
        x_err[1,i]=np.max(x[i]) - x_med[i]
        x_err_tot[i]=x_err[1,i] - x_err[0,i]
-       y[i]=y_df[i] # Esto parece que no tiene sentido pero es para arreglar la dimensión de y. Size(y_df)=(26,1) y Size(y)=(26,).
+       y_OBS[i]=y_df[i] # Esto parece que no tiene sentido pero es para arreglar la dimensión de y. Size(y_df)=(26,1) y Size(y)=(26,).
+       x_OBS[i]= x_OBS_df[i]
        
     # Ajuste:
     
     def modelo(x, a, b):
         return a*x+ b
     
-    parametros_iniciales  = [0.5,0]
+    #parametros_iniciales_Hach  = [0.5,0]
+    #parametros_iniciales_OBS  = [0.5,0]
     
-    popt, pcov = curve_fit(modelo, x_med, y, p0=None)    
+    popt_Hach, pcov_Hach = curve_fit(modelo, x_med, y_Hach, p0=None)    
+    popt_OBS, pcov_OBS = curve_fit(modelo, x_OBS, y_OBS, p0=None)
     
-    pstd = np.sqrt(np.diag(pcov))
+    pstd_Hach = np.sqrt(np.diag(pcov_Hach))
+    pstd_OBS = np.sqrt(np.diag(pcov_OBS))
+    
+    
     nombres_de_param = ['a', 'b']
 
-    print('Resultado del ajuste:\n')
-    for c, v in enumerate(popt):
-        print('%s = %5.4f ± %5.4f' % (nombres_de_param[c], v, pstd[c]/2))
+    print('\nResultado del ajuste (Hach):\n')
+    for c, v in enumerate(popt_Hach):
+        print('%s = %5.4f ± %5.4f' % (nombres_de_param[c], v, pstd_Hach[c]/2))
     
-    plt.errorbar(x_med, y, xerr=x_err, yerr=None, fmt='o', label=r'')#, ms=5.5, capsize=4, zorder=2)
-    plt.plot(x_med, modelo(x_med, *popt), 'r-', label=r'Ajuste: $y = %.4f \; x + %.4f $'%(popt[0],popt[1]))#, lw=2.5, zorder=4)
+    print('\nResultado del ajuste (OBS):\n')
+    for c, v in enumerate(popt_OBS):
+        print('%s = %5.4f ± %5.4f' % (nombres_de_param[c], v, pstd_OBS[c]/2))
+    
+    plt.errorbar(x_med, y_Hach, xerr=x_err, yerr=None, fmt='o',color='darkred', label=r'Hach', ms=5.5, zorder=0)
+    plt.plot(x_med, modelo(x_med, *popt_Hach), color='red', label=r'Ajuste: $y = %.4f \; x + %.4f $'%(popt_Hach[0],popt_Hach[1]), lw=1, zorder=4)
+
+    plt.errorbar(x_OBS, y_OBS, xerr=xerr_OBS, yerr=None, fmt='o', color='darkblue', label=r'OBS', ms=5.5, zorder=0)
+    plt.plot(x_OBS, modelo(x_OBS, *popt_OBS),  color='blue', label=r'Ajuste: $y = %.4f \; x %.4f $'%(popt_OBS[0],popt_OBS[1]), lw=1, zorder=4)
+
 
     plt.tick_params(axis='both', which='major', labelsize=NumberSize)
     plt.xlabel(r'Turbidez (FNU)', fontsize=AxisLabelSize)
     plt.ylabel(r'Concentraci\'on (mg/l)', fontsize=AxisLabelSize)
-    plt.title(r'', fontsize=TitleSize)
-    plt.legend(loc='upper left', fontsize=LegendSize)
+    plt.title(r'OBS', fontsize=TitleSize)
+    plt.legend(loc='best', fontsize=LegendSize)
     plt.grid(axis='both', color='k', linestyle='dashed', linewidth=2, alpha=0.1)
     plt.show()
     plt.savefig(path+'/'+s+'.png')
