cea8790cee8ae54364dda0a8b2201a2be1fecfa4
ScheinerS <santiagoscheiner@gmail.com>
2019-09-24 20:41:06 +0000

    Unificada la carpeta Test.

--- SC6/SC6.py
+++ SC6/SC6.py
@@ -8,8 +8,6 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.odr import Model,RealData,ODR
-from matplotlib.ticker import AutoMinorLocator
 
 path = os.path.dirname(os.path.realpath('__file__'))
 sys.path.append(path)
