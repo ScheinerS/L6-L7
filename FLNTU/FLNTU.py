4c23f212fd21d8c504a6f2742c4fd5c9f6f58053
ScheinerS <santiagoscheiner@gmail.com>
2019-10-25 18:38:31 +0000

    Terminamos los datos del FLNTU.

--- FLNTU/FLNTU.py
+++ FLNTU/FLNTU.py
@@ -29,15 +29,31 @@ plt.rc('font', family='serif')
 
 files = glob.glob(path + '/Datos/Pruebas 23-10-19/' + 'estación' + '*.csv') # Identifica la cantidad de archivos que se quieren leer.
 
-data = {}   # Diccionario con los DataFrames de cada archivo. 
-'''
-# HACH
-hach = {}
+tabla = pd.DataFrame()  # DataFrame donde vamos a guardar los datos de hach, ntu y fl.
 
-# FLNTU
-ntu = {}
-fl = {}
-'''
+
+
+#%%
+
+filename_hach = path + '/Datos/' + 'Turbidez(Hach-FLNTU) - Hoja 1.csv'
+
+hach = pd.read_csv(filename_hach, delimiter = ",", skiprows=1, header=None)
+hach.set_index(0)
+
+
+tabla['hach'] = hach[5]
+tabla['hach_err'] = hach[[2,3,4]].max(axis=1) - hach[[2,3,4]].min(axis=1)
+
+tabla.set_index(hach[0])
+#%%
+data = {}   # Diccionario con los DataFrames de cada archivo. 
+
+tabla['ntu'] = None
+tabla['ntu_err'] = None
+tabla['fl'] = None
+tabla['fl_err'] = None
+
+# Lectura de los archivos del FLNTU:
 for file in files:
     
     if os.name == 'posix':   # Si es Linux.
@@ -50,14 +66,16 @@ for file in files:
     data[filename] = pd.read_csv(file, delimiter="\t", skiprows=1, header=None)
     #data[filename] = data[filename].dropna()
     
-    ntu = data[filename][3]
-    fl = data[filename][5]
+    NTU = data[filename][3] # datos de turbidez
+    FL = data[filename][5]  # datos de fluorecencia
     
-    ntu_mean = np.mean(ntu)
-    ntu_err = np.std(ntu)/np.sqrt(len(ntu))
+    tabla['ntu'].append(pd.Series(np.mean(NTU)))
+    tabla['ntu_err'].append(np.std(NTU)/np.sqrt(len(NTU)))
     
-    fl_mean = np.mean(fl)
-    fl_err = np.std(fl)/np.sqrt(len(fl))
+    tabla.set_value('ntu', filename, 1000)
+    #tabla['fl'] = np.mean(FL)
+    #tabla['fl_err'] = np.std(FL)/np.sqrt(len(FL))
+    
 
     # Falta guardar el valor de cada archivo en un único vector.
     
