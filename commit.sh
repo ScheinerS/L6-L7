0c31bbdbb6cc41851a57473216451654b1037586
Santiago <santiagoscheiner@gmail.com>
2020-03-26 18:36:09 -0300

    Agrego archivos para usar git.

---
+++ commit.sh
@@ -0,0 +1,8 @@
+# Para guardar los cambios locales en el repositorio online:
+
+git init
+git pull
+git add -A
+git commit
+git push
+cd -
---
+++ update.sh
@@ -0,0 +1,9 @@
+# Para actualizar el repositorio local desde el repositorio online:
+
+# La primera vez, abrir la terminal donde luego va a estar el repositorio y usar:
+# git clone https://github.com/ScheinerS/L6-L7
+
+git init
+git pull
+git status
+
