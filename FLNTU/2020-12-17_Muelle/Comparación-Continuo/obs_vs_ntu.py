faad520146f48a658d64166ffa33a590315f4c66
ScheinerS <santiagoscheiner@gmail.com>
2020-05-29 19:46:06 -0300

    Agregué los valores de las estaciones.

--- FLNTU/2020-12-17_Muelle/Comparación-Continuo/obs_vs_ntu.py
+++ FLNTU/2020-12-17_Muelle/Comparación-Continuo/obs_vs_ntu.py
@@ -16,21 +16,19 @@ NumberSize = 12
 
 plt.close('all')
 
-plt.rc('text', usetex=True)
+plt.rc('text', usetex=False)
 plt.rc('font', family='serif')
 
 
 #%%
 obs_vs_ntu = np.loadtxt('obs_ss__(fnu)vs_eco_(ntu).csv', skiprows=1, delimiter=',', unpack=True)
 
-obs=obs_vs_ntu[0]
-eco=obs_vs_ntu[1]
+obs = obs_vs_ntu[0]
+eco = obs_vs_ntu[1]
 
-#%%
-
-# Eliminación de outliers:
-
-
+stations = np.loadtxt('OBS_and_ECO_stations.csv', skiprows=1, delimiter=',', unpack=True)
+obs_stations = stations[0]
+eco_stations = stations[1]
 
 
 #%%
@@ -47,14 +45,14 @@ m=popt[0]
 b=popt[1]
 
 
-
 print('\nAjuste lineal (y=mx+b):\nm=%2.4f\tb=%2.4f'%(m,b))
 
-
+plt.figure()
 plt.plot(obs, eco, '.')
+plt.plot(obs_stations, eco_stations, '.k')
 
 x_modelo = np.linspace(0, 70, 1000)
-plt.plot(x_modelo,modelo(x_modelo, *popt),label=r'$y=%.4g+%.4g$'%(m,b))
+plt.plot(x_modelo,modelo(x_modelo, *popt), color='blue', label=r'$y=%.4g+%.4g$'%(m,b))
 
 
 plt.legend(loc='best', fontsize=LegendSize)
